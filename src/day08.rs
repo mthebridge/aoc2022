c26b114817811e0292af232d371b26be5713f5b6
Mark Thebridge <mark.thebridge@metaswitch.com>
2022-12-08 09:56:45 +0000

    Day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,13 @@
+fn score_range<R, P>(range: &mut R, filter: P) -> usize
+where
+    R: Iterator<Item = usize> + Clone,
+    P: Fn(usize) -> bool,
+{
+    let max_distance = range.clone().count();
+    let short_count = range.take_while(|i| filter(*i)).count();
+    short_count + if max_distance == short_count { 0 } else { 1 }
+}
+
 pub fn run() {
     let input = if std::env::var("AOC_TEST").is_ok() {
         include_str!("../inputs/test08.txt")
@@ -29,15 +39,39 @@ pub fn run() {
                     // Trees on the edge are always visible
                     x == 0 || x == max_x - 1 || y == 0 || y == max_y - 1 ||
                     // Check if all trees in any direction are shorter than this one.
+
                     (0..x).all(|i| heights[y][i] < this_height)
                         || (x + 1..max_x).all(|i| heights[y][i] < this_height)
                         || (0..y).all(|j| heights[j][x] < this_height)
                         || (y + 1..max_y).all(|j| heights[j][x] < this_height)
-                }).count() // Count the number of visible trees in this row
+                })
+                .count() // Count the number of visible trees in this row
         })
         .sum(); // Sum across all rows.
-    let part2 = 0;
+
     println!("Part 1: {}", part1);
+
+    // Calculate "score".  Similar to part1.
+    let part2 = heights
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(x, &this_height)| {
+                    // Check the score for each direction. Score is number in order in that direction
+                    // that are smaller than this one.
+
+                    score_range(&mut (0..x).rev(), |i| heights[y][i] < this_height)
+                        * score_range(&mut (x + 1..max_x), |i| heights[y][i] < this_height)
+                        * score_range(&mut (0..y).rev(), |j| heights[j][x] < this_height)
+                        * score_range(&mut (y + 1..max_y), |j| heights[j][x] < this_height)
+                })
+                .max()
+                .unwrap() // Count the number of visible trees in this row
+        })
+        .max()
+        .unwrap();
 
     println!("Part 2: {}", part2)
 }
